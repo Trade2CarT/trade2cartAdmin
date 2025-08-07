166f663771381c1835166f9d91feb06a6a13758f
Imran S <trade2cart@gmail.com>
2025-08-07 11:11:51 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    tailwindcss(), react()], build: {
-      rollupOptions: {
-        external: ['firebase/database']  // 👈 Add this line
-      }
-    }
-
+    tailwindcss(), react()],
  
 })
